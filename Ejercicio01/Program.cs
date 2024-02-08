40d746a0ab2a5d9949e577047b5c92ff68338b60
TecNM <TecNM@DESKTOP-BABKH5A>
2024-02-08 16:59:09 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Exercise #1. Enter degrees Celsius -> the program will convert them into Farenheit and Kelvin
+//kelvin = celsius + 273
+//fahrenheit = celsius x 18/10 + 32
+double Degrees, F, K;
+Console.Write("Enter the degrees °C:");
+Degrees = Convert.ToDouble(Console.ReadLine());
+F = ((Degrees * 18) / 10) + 32;
+K = Degrees + 273;
+Console.WriteLine("F°:"+F);
+Console.WriteLine("K°:" + K);
+Console.ReadKey();
